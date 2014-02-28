d32154f48d78d637f68be0fa60392cd96f4fa0a7
Ben Postlethwaite <post.ben.here@gmail.com>
2014-02-28 17:10:21 -0500

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 (function () {
 var multilevel = require('multilevel')
   , reconnect = require('reconnect')
-   // , config = require("./config.json")
+  , config = require("./config.json")
   , manifest = require("./manifest.json")
   , db = multilevel.client(manifest)
 
@@ -30,7 +30,7 @@ reconnect( function (stream) {
 
 
 })
-.connect(9999, "ec2-174-129-51-152.compute-1.amazonaws.com") //)
+.connect(9999, config.dbhost)
 .on("connect", function () {
   if (LPanels && LPanels.length) {
     LPanels.forEach( function (logpanel) {
@@ -48,11 +48,11 @@ var logpanels = document.querySelectorAll(".log-panel-container")
   ,  LPanels = []
 
     LPanels.push( LogPanel(logpanels[0], db)
-                .addFilter("service", "SOURCE"))
-    LPanels.push( LogPanel(logpanels[1], db))
-  //               .addFilter("service", "WEBSOCKET"))
-    LPanels.push( LogPanel(logpanels[2], db))
-  //               .addFilter("service", "RESTFUL"))
-    LPanels.push( LogPanel(logpanels[3], db))
-  //               .addFilter("service", "false"))
+                  .addFilter("service", "SOURCE"))
+    LPanels.push( LogPanel(logpanels[1], db)
+                  .addFilter("service", "WEBSOCKET"))
+    LPanels.push( LogPanel(logpanels[2], db)
+                  .addFilter("service", "RESTFUL"))
+    LPanels.push( LogPanel(logpanels[3], db)
+                  .addFilter("service", "false"))
 })()
